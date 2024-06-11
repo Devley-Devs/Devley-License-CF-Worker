67e4214d70571f5b2f25c0b4aec2e4f57eb38e0f
yuvaraja28 <yuvaraja28042005@gmail.com>
2024-06-12 05:11:58 +0530

    Live Websocket Feature in License Servers

--- src/worker.js
+++ src/worker.js
@@ -4,6 +4,7 @@ import * as Realm from 'realm-web';
 export default {
 	async fetch(request, env) {
 		const ip = request.headers.get('CF-Connecting-IP')
+    const upgradeHeader = request.headers.get('Upgrade');
 		if (request.method === "POST") {
 			if (!(request.headers.get("Authorization") === `Bearer ${env.AUTHORIZATION_HEADER}`)) {
 				const request_body = await request.json()
@@ -24,8 +25,8 @@ export default {
           var user = await app.logIn(Realm.Credentials.apiKey(env.REALM_API));
           var client = user.mongoClient('mongodb-atlas');
           var Licenses = client.db('DevleyDB').collection('Licenses')
-          var ProductActivities = client.db('DevleyDB').collection('ProductActivities')
           var Products = client.db('DevleyDB').collection('Products')
+          var ProductActivities = client.db('DevleyDB').collection('ProductActivities')
         } catch(err) {
           return new Response(JSON.stringify({'status': `Database Error`}), {status: 500});
         }
@@ -55,7 +56,7 @@ export default {
 				} else if (db_data?.blocked) {
           embed = {...embed, fields: embed_fields, description: "### `⛔` Blocked License Key", timestamp: new Date().toISOString(), color: 16390168}
           if (String(env.OWNER_ID) != String(user_id)) {
-            await ProductActivities.insertOne({ user_id: user_id, product_id: env.PRODUCT_ID, version: request_body.version, time: new Date(), ip: ip, status: '⛔' })
+            await ProductActivities.insertOne({ user_id: user_id, product_id: env.PRODUCT_ID, version: request_body.version, time: new Date(), ip: ip, machine_id: request_body?.machine_id, status: '⛔' })
             try { await fetch(env.DISCORD_WEBHOOK, { method: 'POST', headers: {"Content-Type": 'application/json'}, body: JSON.stringify({ embeds: [embed] })}) }
             catch {}
           }
@@ -63,7 +64,7 @@ export default {
         } else {
           embed = {...embed, fields: embed_fields, description: "### `✅` Successfully sent the License Key", timestamp: new Date().toISOString(), color: env.COLOR}
           if (String(env.OWNER_ID) != String(user_id)) {
-            await ProductActivities.insertOne({ user_id: user_id, product_id: env.PRODUCT_ID, version: request_body.version, time: new Date(), ip: ip, status: '✅' })
+            await ProductActivities.insertOne({ user_id: user_id, product_id: env.PRODUCT_ID, version: request_body.version, time: new Date(), ip: ip, machine_id: request_body?.machine_id, status: '✅' })
             try { await fetch(env.DISCORD_WEBHOOK, { method: 'POST', headers: {"Content-Type": 'application/json'}, body: JSON.stringify({ embeds: [embed] })}) }
             catch {}
           }
@@ -80,7 +81,91 @@ export default {
         } catch {}
         return new Response(JSON.stringify({error :"UnAuthorized Access"}), {status: 403});
 			}
-		} else if (request.method === "GET") {
+		} else if (upgradeHeader && upgradeHeader === 'websocket') {
+      const webSocketPair = new WebSocketPair();
+      const [ws_client, ws_server] = Object.values(webSocketPair);
+      ws_server.accept();
+      ws_client.auth_success = false
+      ws_client.user_id = null
+      ws_client.version = null
+      ws_client.session_id = null
+      ws_client.machine_id = null
+      try {
+        const app = new Realm.App({ id: env.REALM_ID });
+        var user = await app.logIn(Realm.Credentials.apiKey(env.REALM_API));
+        var client = user.mongoClient('mongodb-atlas');
+        var Licenses = client.db('DevleyDB').collection('Licenses')
+        var OnlineLicenses = client.db('DevleyDB').collection('OnlineLicenses')
+      } catch(err) {
+        return new Response(JSON.stringify({'status': `Database Error`}), {status: 500});
+      }
+      ws_server.addEventListener('message', async (event) => {
+        try {
+          const ws_data = JSON.parse(event.data)
+          console.log(ws_data)
+          if (ws_data.event === 'license_key') {
+            var splitted_data = ws_data.data.split("_")
+            var transaction_id = splitted_data[0]
+            ws_client.version = ws_data.version
+            ws_client.session_id = ws_data.session_id
+            ws_client.machine_id = ws_data.machine_id
+            ws_client.user_id = splitted_data[1]
+            try {
+              var uuid = splitted_data[2]
+            } catch {
+              var uuid = undefined
+            }
+            const licenses = await Licenses.findOne({ _id: String(ws_client.user_id) })
+            const db_data = licenses?.products[env.PRODUCT_ID]
+            if ((db_data != null) && (db_data?.transaction_id == transaction_id) && (db_data?.misc?.uuid == uuid) && (!db_data?.blocked)) {
+              ws_client.auth_success = true
+              ws_server.send(JSON.stringify({'event': 'auth_success', 'keepalive': env.WS_KEEPALIVE}))
+              const previousConnection = await OnlineLicenses.findOneAndUpdate({ session_id: ws_client.session_id }, { user_id: ws_client.user_id, product_id: env.PRODUCT_ID, version: ws_client.version, time: new Date(), ip: ip, session_id: ws_client.session_id, machine_id: ws_client.machine_id })
+              if (previousConnection == null) {
+                await OnlineLicenses.insertOne({ user_id: ws_client.user_id, product_id: env.PRODUCT_ID, version: ws_client.version, time: new Date(), ip: ip, session_id: ws_client.session_id, machine_id: ws_client.machine_id })
+              }
+            } else {
+              ws_server.send(JSON.stringify({'event': 'auth_failed'}))
+              return ws_server.close(1008, "ERROR: Invalid License Key")
+            }
+          } else if ((ws_data.event === 'keepalive') && (ws_client.auth_success)) {
+            ws_client.version = ws_data.version
+            ws_client.session_id = ws_data.session_id
+            ws_client.machine_id = ws_data.machine_id
+            const activeConnections = await OnlineLicenses.find({ user_id: ws_client.user_id })
+            for (let conn in activeConnections) {
+              if (new Date(activeConnections[conn].time.getTime() + ((env.WS_KEEPALIVE)*1000)) < new Date()) {
+                await OnlineLicenses.deleteOne({ session_id: activeConnections[conn].session_id })
+                activeConnections.splice(conn, conn)
+              }
+            }
+            console.log(activeConnections)
+            if (activeConnections.length >= 2) {
+              ws_server.send(JSON.stringify({ 'event': 'killconn' }))
+              return await OnlineLicenses.deleteOne({ session_id: ws_client.session_id })
+            }
+            const previousConnection = await OnlineLicenses.findOneAndUpdate({ session_id: ws_client.session_id }, { user_id: ws_client.user_id, product_id: env.PRODUCT_ID, version: ws_client.version, time: new Date(), ip: ip, session_id: ws_client.session_id, machine_id: ws_client.machine_id })
+            if (previousConnection == null) {
+              await OnlineLicenses.insertOne({ user_id: ws_client.user_id, product_id: env.PRODUCT_ID, version: ws_client.version, time: new Date(), ip: ip, session_id: ws_client.session_id, machine_id: ws_client.machine_id })
+            }
+            ws_server.send(JSON.stringify({ 'event': 'keepalive' }))
+          }
+        } catch {
+          ws_server.send(JSON.stringify({'event': "error", "data": "License Key in Wrong Format"}));
+          return ws_server.close(1008, "ERROR: Invalid License Key Format")
+        }
+      });
+      ws_server.addEventListener('close', async (event) => {
+        if ((ws_client.auth_success) && (ws_client.session_id)) {
+          await OnlineLicenses.deleteOne({ session_id: ws_client.session_id })
+        }
+        return ws_server.close(1000, "Closing the License Websocket Connection")
+      });
+      return new Response(null, {
+        status: 101,
+        webSocket: ws_client,
+      });
+    } else if (request.method === "GET") {
 			return new Response(`${env.PRODUCT_NAME} Licensing System V${env.VERSION}`, {status: 302});
 		}
 	},
