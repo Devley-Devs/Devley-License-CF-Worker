e0ac365d59550a85425ad9421415788a9d4d7f25
yuvaraja28 <yuvaraja28042005@gmail.com>
2024-11-14 05:31:06 +0530

    Restricting Path

--- src/worker.js
+++ src/worker.js
@@ -4,20 +4,26 @@ import { LicenseHTTP } from './methods/http';
 export default {
 	async fetch(request, env) {
     const ip = request.headers.get('CF-Connecting-IP')
-    const upgradeHeader = request.headers.get('Upgrade');
-		if (request.method === "POST") {
-      return await LicenseHTTP(request, ip, env)
-    } else if (upgradeHeader && upgradeHeader === 'websocket') {
-      const { ws_client, response } = await LicenseWS(ip, env)
-      if (response) {
-        return response;
+    try {
+      const url = new URL(request.url)
+      const upgradeHeader = request.headers.get('Upgrade');
+      if ((request.method === "POST") && (url.pathname == '/')) {
+        return await LicenseHTTP(request, ip, env)
+      } else if (upgradeHeader && upgradeHeader === 'websocket') {
+        const { ws_client, response } = await LicenseWS(ip, env)
+        if (response) {
+          return response;
+        }
+        return new Response(null, {
+          status: 101,
+          webSocket: ws_client,
+        });
+      } else if (request.method === "GET") {
+        return new Response(`${env.PRODUCT_NAME} Licensing System V${env.VERSION}`, {status: 302});
       }
-      return new Response(null, {
-        status: 101,
-        webSocket: ws_client,
-      });
-    } else if (request.method === "GET") {
-			return new Response(`${env.PRODUCT_NAME} Licensing System V${env.VERSION}`, {status: 302});
-		}
+    } catch (err) {
+      console.log("ERROR OCCURED", ip, err)
+      return new Response(`${env.PRODUCT_NAME} Licensing System V${env.VERSION}`, {status: 302});
+    }
 	},
 };
